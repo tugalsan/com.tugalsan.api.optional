e4b6cdeed8e8b8cb1897c32f59a8324c3542ea8f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-10-02 09:21:25 +0300

    Update TGS_Optional.java

--- src/main/java/com/tugalsan/api/optional/client/TGS_Optional.java
+++ src/main/java/com/tugalsan/api/optional/client/TGS_Optional.java
@@ -6,9 +6,21 @@ import java.util.Optional;
 
 public class TGS_Optional<T> {
 
-	private TGS_Optional(T value) {
-		payload = Optional.of(value);
-	}
-	final Optional payload;
-	public List<? extends CharSequence> info = new ArrayList();
+    private TGS_Optional() {
+        payload = Optional.empty();
+    }
+
+    private TGS_Optional(T value) {
+        payload = Optional.of(value);
+    }
+    final public Optional payload;
+    public List<? extends CharSequence> info = new ArrayList();
+
+    public static <T> TGS_Optional<T> of(T value) {
+        return new TGS_Optional(value);
+    }
+
+    public static <T> TGS_Optional<T> ofEmpty() {
+        return new TGS_Optional();
+    }
 }
