3990805aef4297b8b7a877dd89e301ea0be8110f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-10-02 09:19:41 +0300

    Update TGS_Optional.java

--- src/main/java/com/tugalsan/api/optional/client/TGS_Optional.java
+++ src/main/java/com/tugalsan/api/optional/client/TGS_Optional.java
@@ -1,9 +1,10 @@
 package com.tugalsan.api.optional.client;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class class TGS_Optional<T> {
+public class TGS_Optional<T> {
 
 	private TGS_Optional(T value) {
 		payload = Optional.of(value);
